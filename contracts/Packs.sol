40f2287c330dfd597363a98cb51bce50e6103996
illestrater <illestrater@gmail.com>
2021-07-07 15:18:13 -0700

    Base64 and mint function declarations

--- contracts/Packs.sol
+++ contracts/Packs.sol
@@ -19,13 +19,16 @@ contract Relics is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
   bytes32[] public descriptions;
   bytes32[] public assets;
   uint256[] public counts;
+  uint256[] public soldCounts;
   uint256 public tokenPrice;
   uint256 public bulkBuyLimit;
   uint256 public maxSupply;
   bool public editioned;
-  bool public randomEditions;
   uint256 public saleStartTime;
   string public licenseURI;
+
+  mapping (uint256 => uint256) internal _minted;
+  uint256 internal _mintPointer = 0;
 
   constructor(
     address payable _daoAddress,
@@ -37,7 +40,6 @@ contract Relics is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
     bytes32[] memory _assets,
     uint256[] memory _counts,
     bool _editioned,
-    bool _randomEditions,
     uint256 _bulkBuyLimit,
     uint256 _saleStartTime,
     string memory _licenseURI
@@ -50,16 +52,18 @@ contract Relics is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
     assets = _assets;
     counts = _counts;
     editioned = _editioned;
-    randomEditions = _randomEditions;
     bulkBuyLimit = _bulkBuyLimit;
     saleStartTime = _saleStartTime;
     licenseURI = _licenseURI;
 
     uint256 _count = 0;
+    uint256[] memory _soldCounts = uint256[](_counts.length);
     for (uint i = 0; i < _counts.length; i++) {
       _count = _count + _counts[i];
+      _soldCounts[i] = 0;
     }
 
+    soldCounts = _soldCounts;
     maxSupply = _count;
   }
 
@@ -68,13 +72,28 @@ contract Relics is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
     _;
   }
 
-  function mint() public override payable nonReentrant {
-    require(_tokenIdTracker.current() < maxSupply, "Total supply reached");
+  function random() private view returns (uint) {
+    return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    // random()%assets.length
+  }
 
-    _tokenIdTracker.increment();
+  /** 
+   * Map token order w/ URI upon mints
+   * Sample token ID (edition #77) with collection of 12 different assets: 1200077
+   */
+
+   // 10
+   // 50
+   // 20
+  function mint() public override payable nonReentrant {
+    uint256 i = 0;
+
+    while (uint256 i = 0; i < _assets.length; i++) {
+
+    }
 
     uint256 tokenId = _tokenIdTracker.current();
-    
+
     if (daoInitialized) {
       (bool transferToDaoStatus, ) = daoAddress.call{value:tokenPrice}("");
       require(transferToDaoStatus, "Address: unable to send value, recipient may have reverted");
@@ -82,8 +101,8 @@ contract Relics is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
 
     uint256 excessAmount = msg.value.sub(tokenPrice);
     if (excessAmount > 0) {
-        (bool returnExcessStatus, ) = _msgSender().call{value: excessAmount}("");
-        require(returnExcessStatus, "Failed to return excess.");
+      (bool returnExcessStatus, ) = _msgSender().call{value: excessAmount}("");
+      require(returnExcessStatus, "Failed to return excess.");
     }
 
     _mint(_msgSender(), tokenId);
@@ -113,5 +132,12 @@ contract Relics is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
 
   function lastTokenId() public override view returns (uint256 tokenId) {
     return _tokenIdTracker.current();
+  }
+
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    string memory baseURI = _baseURI();
+    return bytes(baseURI).length > 0
+        ? string(abi.encodePacked(baseURI, tokenId.toString()))
+        : '';
   }
 }
