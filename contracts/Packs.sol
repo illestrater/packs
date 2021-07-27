d8e81a57e2e90c9bad57733a4a84f182b443871f
illestrater <illestrater@gmail.com>
2021-07-26 19:12:36 -0700

    Add metadata and more tests

--- contracts/Packs.sol
+++ contracts/Packs.sol
@@ -23,17 +23,24 @@ contract Packs is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
 
   mapping (uint256 => string) versionedAssets;
   struct SingleCollectible {
-    string title;
-    string description;
-    uint256 count;
-    uint256 versionCount;
-    uint256 currentVersion;
+    string title; // Collectible name
+    string description; // Collectible description
+    uint256 count; // Amount of editions per collectible
+    uint256 totalVersionCount; // Total number of existing states
+    uint256 currentVersion; // Current existing state
     string[] assets; // Each asset in array is a version
   }
 
-  mapping (uint256 => SingleCollectible) collectibles;
+  struct BasicMetadata {
+    string[] name; // Trait or attribute property field name
+    string[] value; // Trait or attribute property value
+    bool[] modifiable; // Can owner modify the value of field
+    uint256 propertyCount; // Tracker of total attributes
+  }
 
-  uint256[] public shuffleIDs;
+  mapping (uint256 => SingleCollectible) collectibles;
+  mapping (uint256 => BasicMetadata) basicMetadata;
+  mapping (uint256 => string) public licenseURI;
 
   uint256 public collectibleCount;
   uint256 public totalTokenCount;
@@ -41,7 +48,9 @@ contract Packs is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
   uint256 public bulkBuyLimit;
   uint256 public saleStartTime;
   bool public editioned;
-  string public licenseURI;
+  uint256 public licenseVersion;
+
+  uint256[] public shuffleIDs;
 
   constructor(
     string memory name,
@@ -53,9 +62,11 @@ contract Packs is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
     uint256[] memory _counts,
     bool _editioned,
     uint256[] memory _initParams,
+    string[][][] memory _metadataValues,
     string memory _licenseURI
   ) ERC721PresetMinterPauserAutoId(name, symbol, baseURI) public {
     require(_titles.length == _descriptions.length && _titles.length == _assets.length && _titles.length == _counts.length);
+
     daoAddress = msg.sender;
     daoInitialized = false;
 
@@ -71,26 +82,23 @@ contract Packs is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
       uint256 count = 0;
       for (uint256 j = 0; j < bytes(_assets[i]).length; j++) {
         if (bytes(_assets[i])[j] == ",") {
-          // console.log(substringPointer, substring(_assets[i], substringPointer, j));
           _singleAssets[count] = substring(_assets[i], substringPointer, j);
           substringPointer = j + 1;
           count++;
         }
 
         if (j == bytes(_assets[i]).length - 1) {
-          // console.log(substringPointer, substring(_assets[i], substringPointer, j + 1));
           _singleAssets[count] = substring(_assets[i], substringPointer, j + 1);
           substringPointer = j;
           count++;
         }
       }
-      // console.log(substring(_assets[i], substringPointer, j));
 
       collectibles[i] = SingleCollectible({
         title: _titles[i],
         description: _descriptions[i],
         count: _counts[i],
-        versionCount: count,
+        totalVersionCount: count,
         currentVersion: 0,
         assets: _singleAssets
       });
@@ -99,13 +107,32 @@ contract Packs is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
       _totalTokenCount += _counts[i];
     }
 
+    for (uint256 i = 0; i < _metadataValues.length; i++) {
+      string[] memory propertyNames = new string[](_metadataValues[i].length);
+      string[] memory propertyValues = new string[](_metadataValues[i].length);
+      bool[] memory modifiables= new bool[](_metadataValues[i].length);
+      for (uint256 j = 0; j < _metadataValues[i].length; j++) {
+        propertyNames[j] = _metadataValues[i][j][0];
+        propertyValues[j] = _metadataValues[i][j][1];
+        modifiables[j] = (keccak256(abi.encodePacked((_metadataValues[i][j][2]))) == keccak256(abi.encodePacked(('true'))));
+      }
+
+      basicMetadata[i] = BasicMetadata({
+        name: propertyNames,
+        value: propertyValues,
+        modifiable: modifiables,
+        propertyCount: _metadataValues[i].length
+      });
+    }
+
     collectibleCount = _collectibleCount;
     totalTokenCount = _totalTokenCount;
     editioned = _editioned;
     tokenPrice = _initParams[0];
     bulkBuyLimit = _initParams[1];
     saleStartTime = _initParams[2];
-    licenseURI = _licenseURI;
+    licenseURI[0] = _licenseURI;
+    licenseVersion = 1;
 
     createTokenIDs();
   }
@@ -151,7 +178,7 @@ contract Packs is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
   }
 
   // Define current owners of each ID (reference infinfts)
-  function mint() public override payable {
+  function mint() public override payable nonReentrant {
     if (daoInitialized) {
       (bool transferToDaoStatus, ) = daoAddress.call{value:tokenPrice}("");
       require(transferToDaoStatus, "Address: unable to send value, recipient may have reverted");
@@ -194,8 +221,9 @@ contract Packs is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
     }
   }
 
-  function mint(address to) public override(ERC721PresetMinterPauserAutoId) {
-    revert("Should not use this one");
+  function updateMetadata(uint256 collectibleId, uint256 propertyIndex, string memory value) public onlyDAO {
+    require(basicMetadata[collectibleId - 1].modifiable[propertyIndex], 'Metadata field not updateable');
+    basicMetadata[collectibleId - 1].value[propertyIndex] = value;
   }
 
   // Index starts at version 1, collectible 1 (so shifts 1 for 0th index)
@@ -204,17 +232,42 @@ contract Packs is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
   }
 
   function addVersion(uint256 collectibleNumber, string memory asset) public onlyDAO {
-    collectibles[collectibleNumber - 1].assets[collectibles[collectibleNumber - 1].versionCount] = asset;
-    collectibles[collectibleNumber - 1].versionCount++;
+    collectibles[collectibleNumber - 1].assets[collectibles[collectibleNumber - 1].totalVersionCount] = asset;
+    collectibles[collectibleNumber - 1].totalVersionCount++;
+  }
+
+  function addNewLicense(string memory _license) public onlyDAO {
+    licenseURI[licenseVersion] = _license;
+    licenseVersion++;
+  }
+
+  function getLicense() public view returns (string memory) {
+    return licenseURI[licenseVersion - 1];
+  }
+
+  function getLicenseVersion(uint256 versionNumber) public view returns (string memory) {
+    return licenseURI[versionNumber - 1];
   }
 
   function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
     string memory stringId = toString(tokenId);
     uint256 edition = safeParseInt(substring(stringId, bytes(stringId).length - 5, bytes(stringId).length)) - 1;
     uint256 collectibleId = (tokenId - edition) / 100000 - 1;
+    string memory metadata = '';
 
-    return
-      string(
+    for (uint i = 0; i < basicMetadata[collectibleId].propertyCount; i++) {
+      metadata = string(abi.encodePacked(
+        metadata,
+        '{"trait_type":"',
+        basicMetadata[collectibleId].name[i],
+        '", "value":"',
+        basicMetadata[collectibleId].value[i],
+        '"}',
+        i == basicMetadata[collectibleId].propertyCount - 1 ? '' : ',')
+      );
+    }
+
+    string memory encoded = string(
         abi.encodePacked(
           'data:application/json;base64,',
           Base64.encode(
@@ -223,18 +276,22 @@ contract Packs is IPacks, ERC721PresetMinterPauserAutoId, ReentrancyGuard {
                 '{"name":"',
                 collectibles[collectibleId].title,
                 editioned ? ' #' : '',
-                editioned ? toString(edition) : '',
+                editioned ? toString(edition + 1) : '',
                 '", "description":"',
                 collectibles[collectibleId].description,
                 '", "image": "',
                 _baseURI,
                 collectibles[collectibleId].assets[collectibles[collectibleId].currentVersion],
-                '"}'
+                '", "attributes": [',
+                metadata,
+                '] }'
               )
             )
           )
         )
       );
+    
+    return encoded;
   }
 
   function toString(uint256 value) internal pure returns (string memory) {
--- test/packs-test.js
+++ test/packs-test.js
@@ -6,12 +6,26 @@ function base64toJSON(string) {
 }
 
 describe("Greeter", function() {
-  let tokenPrice = ethers.utils.parseEther("0.0777");
-  let packsInstance;
-  let tokenCounts = [10, 50, 20];
-  let totalTokenCount = 0;
+  const baseURI = 'https://arweave.net/';
+  const tokenPrice = ethers.utils.parseEther("0.0777");
+  const tokenCounts = [10, 50, 20];
+  const tokenNames = ['First name', 'Name two', 'Thirds'];
+  const descriptions = ['The first description', 'Second descript', 'Third yooooo'];
+  const metadata = [
+    [
+      ['test','val1', 'true'],
+      ['yoooo', 'val2', 'false']
+    ],
+    [
+      ['test2','val3', 'true'],
+      ['yoooo2', 'val4', 'false']
+    ],
+  ];
   
+  let totalTokenCount = 0;
   tokenCounts.forEach(e => totalTokenCount += e);
+  
+  let packsInstance;
 
   before(async () => {
     const Packs = await ethers.getContractFactory("Packs");
@@ -19,14 +33,15 @@ describe("Greeter", function() {
       // '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
       'Relics',
       'MONSTERCAT',
-      'https://arweave.net/',
-      ['First name', 'Name two', 'Thirds'],
-      ['The first description', 'Second descript', 'Third yooooo'],
+      baseURI,
+      tokenNames,
+      descriptions,
       ['one,two,three','ayyyooo,twooooo', 'hello'],
       tokenCounts,
       true,
       [tokenPrice, 50, 1948372],
-      'https://arweave.net/license'
+      metadata,
+      'https://arweave.net/license',
     );
     await packsInstance.deployed();
   });
@@ -37,9 +52,13 @@ describe("Greeter", function() {
 
   it("should mint one token", async function() {
     await packsInstance.functions['mint()']({value: tokenPrice})
-
     expect((await packsInstance.getTokens()).length).to.equal(totalTokenCount - 1);
   });
+
+  it("should reject mints with insufficient funds", async function() {
+    expect(packsInstance.functions['mint()']({value: tokenPrice.div(2) })).to.be.reverted;
+    expect(packsInstance.bulkMint(50, {value: tokenPrice.mul(49) })).to.be.reverted;
+  })
 
   it("should bulk mint all tokens", async function() {
     expect(packsInstance.bulkMint(10000, {value: tokenPrice.mul(10000) })).to.be.reverted;
@@ -54,10 +73,48 @@ describe("Greeter", function() {
     expect(await packsInstance.ownerOf(100001)).to.equal(owner.address);
   });
 
-  it("first token ", async function() {
+  it("metadata should match and be updated", async function() {
+    const yo = await packsInstance.tokenURI(100008);
+    const tokenJSON = base64toJSON(yo);
+    expect(tokenJSON.name).to.equal(`${ tokenNames[0] } #8`);
+    expect(tokenJSON.description).to.equal(descriptions[0]);
+    expect(tokenJSON.image).to.equal(`${ baseURI }one`);
+    expect(tokenJSON.attributes[0].trait_type).to.equal(`test`);
+    expect(tokenJSON.attributes[0].value).to.equal(`val1`);
+  });
+
+  it ("should update metadata", async function() {
+    await packsInstance.updateMetadata(1, 0, 'new new')
+    const yo = await packsInstance.tokenURI(100008);
+    const tokenJSON = base64toJSON(yo);
+    expect(tokenJSON.attributes[0].trait_type).to.equal(`test`);
+    expect(tokenJSON.attributes[0].value).to.equal(`new new`);
+  });
+
+  it ("should not be able to update permanent metadata", async function() {
+    expect(packsInstance.updateMetadata(1, 1, 'should not update')).to.be.reverted;
+  })
+
+  it("should update image asset and version", async function() {
     await packsInstance.addVersion(1, 'fourrrrrrr');
     await packsInstance.updateVersion(1, 4);
-    const yo = await packsInstance.tokenURI(100008);
-    console.log(base64toJSON(yo));
+    const tokenJSON = base64toJSON(await packsInstance.tokenURI(100008));
+    expect(tokenJSON.image).to.equal(`${ baseURI }fourrrrrrr`);
   });
+
+  it("should add new license version", async function() {
+    const license = await packsInstance.getLicense();
+    expect(license).to.equal('https://arweave.net/license');
+
+    await packsInstance.addNewLicense('https://arweave.net/new-license');
+    const updatedLicense = await packsInstance.getLicense();
+    expect(updatedLicense).to.equal('https://arweave.net/new-license');
+  });
+
+  it("should have original license", async function() {
+    const license = await packsInstance.getLicenseVersion(1);
+    expect(license).to.equal('https://arweave.net/license');
+  })
+
+  /* TODO: Write test to check non-editioned names */
 });
